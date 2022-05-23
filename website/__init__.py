e0e83e6804df66d80137e06240171f6ea8139f61
Jayesh Rohan Singh <jaysingh@redhat.com>
2022-05-23 09:44:05 +0530

    [added] user accounts

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,38 @@
 import cv2
 from flask import Flask
+from flask_pymongo import PyMongo
+from flask_login import LoginManager
+from .constants import init_constants
+
+
+mongo = PyMongo()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["MONGO_URI"] = init_constants["MONGO_URI"]
+    app.config["SECRET_KEY"] = init_constants["APP_CONFIG_SECRET_KEY"]
+
+    mongo.init_app(app)
+
+    from .auth import auth
+    from .views import views
+
+    app.register_blueprint(auth, url_prefix="/")
+    app.register_blueprint(views, url_prefix="/")
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    from .user import get_user
+
+    @login_manager.user_loader
+    def load_user(email):
+        return get_user(email)
+
+    return app
 
 
 def get_camera():
@@ -8,27 +41,3 @@ def get_camera():
         if cam and cam.isOpened():
             return cam
     raise ValueError("Camera Not Found")
-
-
-camera = get_camera()
-
-
-def create_app():
-    app = Flask(__name__)
-
-    from .views import views
-
-    app.register_blueprint(views, url_prefix="/")
-
-    return app
-
-
-def gen_frames():
-    while True:
-        success, frame = camera.read()
-        if not success:
-            break
-        frame = cv2.flip(frame, 1)
-        _, buffer = cv2.imencode(".jpg", frame)
-        frame = buffer.tobytes()
-        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
---
+++ website/constants.py
@@ -0,0 +1,9 @@
+# Constants to be used in website files
+# Dict name corresponds to the respective file name
+# NOTE : Key should always be in ALL CAPS
+# Add new constants at the end of the corresponding dictionary
+
+init_constants = {
+    "APP_CONFIG_SECRET_KEY": "kjsdifajefpoiannsaniucak",
+    "MONGO_URI": "mongodb+srv://admin:batch2022@cluster0.vp7kz.mongodb.net/HRT?retryWrites=true&w=majority"
+}
